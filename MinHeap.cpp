8d6a192e4c5fd8927a89907b780c842ae74dfbeb
Felipoto <felipe.rojo@alumnos.ucn.cl>
2024-11-24 19:35:17 -0300

    feat: se implemento MinHeap.cpp y MinHeap.h, queda corregir errores

--- MinHeap.cpp
+++ MinHeap.cpp
@@ -1,3 +1,58 @@
 //
-// Created by felip on 24-11-2024.
+// Created by felip on 19-11-2024.
 //
+
+
+#include "MinHeap.h"
+
+//Constructor
+MinHeap::MinHeap() = default;
+
+//Verifica si el MinHeap esta vacio
+bool MinHeap::estaVacio() const {
+    return heap.empty();
+}
+
+//Inserta un pedido en el MinHeap
+void MinHeap::insertarPedido(const Pedido& pedido, int prioridad) {
+    NodoHeap nuevoNodo(pedido, prioridad);
+
+    //Usamos una cola temporal para reorganizar los elementos
+    std::queue<NodoHeap> tempQueue;
+
+    //Insertamos el nuevo nodo en su posicion
+    bool insertado = false;
+    while (!heap.empty()) {
+        NodoHeap actual = heap.front();
+        heap.pop();
+
+        //Insertamos el nuevo nodo de acuerdo a su prioridad
+        if (!insertado && nuevoNodo.prioridad < actual.prioridad) {
+            tempQueue.push(nuevoNodo);
+            insertado = true;
+        }
+
+        tempQueue.push(actual);
+    }
+
+    //Si no se inserto, se agrega al final
+    if (!insertado) {
+        tempQueue.push(nuevoNodo);
+    }
+
+    //Restauramos la cola ordenada al heap
+    heap = tempQueue;
+}
+
+//Extrae el pedido con la minima prioridad
+Pedido MinHeap::extraerMin() {
+    if (estaVacio()) {
+        std::cerr << "El MinHeap esta vacio \n";
+        return Pedido();
+    }
+
+    NodoHeap minNodo = heap.front();
+    heap.pop();
+
+    return minNodo.pedido;
+}
--- MinHeap.h
+++ MinHeap.h
@@ -1,8 +1,34 @@
 //
-// Created by felip on 24-11-2024.
+// Created by felip on 19-11-2024.
 //
 
 #ifndef MINHEAP_H
 #define MINHEAP_H
 
-#endif //MINHEAP_H
+#include "Pedido.h"
+#include <queue>
+#include <iostream>
+
+class MinHeap {
+private:
+    struct NodoHeap {
+        Pedido pedido;
+        int prioridad;
+
+        NodoHeap(const Pedido& pedido, int prioridad) : pedido(pedido), prioridad(prioridad) {}
+    };
+
+    std::queue<NodoHeap> heap;
+
+    //Funcion para reorganizar los elementos en forma de MinHeap
+    void reorganizar();
+
+public:
+    MinHeap(); //Constructor vacio
+
+    void insertarPedido(const Pedido& pedido, int prioridad);
+    Pedido extraerMin();
+    bool estaVacio() const;
+};
+
+#endif // MINHEAP_H
