3c92617ee7b007945aa561bf29938cb009a7dfd0
Felipoto <felipe.rojo@alumnos.ucn.cl>
2024-11-24 15:24:03 -0300

    Fix: se implemento Pedido.cpp y Pedido.h, corrigiendo errores

--- Pedido.h
+++ Pedido.h
@@ -1,8 +1,46 @@
 //
-// Created by felip on 24-11-2024.
+// Created by felip on 18-11-2024.
 //
 
 #ifndef PEDIDO_H
 #define PEDIDO_H
 
-#endif //PEDIDO_H
+#include <string>
+#include <list>
+
+class Pedido {
+private:
+    int id;
+    std::string nombreCliente;
+    std::string apellidoCliente;
+    std::list<std::string> productos;
+    std::string horaPedido;
+    bool enPreparacion;
+    bool completado;
+
+public:
+    //Constructor vacio
+    Pedido();
+
+    //Constructor de pedido
+    Pedido(int id, std::string nombreCliente, std::string apellidoCliente,
+           std::list<std::string> productos, std::string horaPedido);
+
+    //Getters y setters
+    [[nodiscard]] int getId() const;
+    [[nodiscard]] const std::string& getNombreCliente() const;
+    [[nodiscard]] const std::string& getApellidoCliente() const;
+    [[nodiscard]] const std::list<std::string>& getProductos() const;
+    [[nodiscard]] const std::string& getHoraPedido() const;
+    [[nodiscard]] bool isEnPreparacion() const;
+    [[nodiscard]] bool isCompletado() const;
+
+    void setNombreCliente(std::string nombreCliente);
+    void setApellidoCliente(std::string apellidoCliente);
+    void setProductos(std::list<std::string> productos);
+    void setHoraPedido(std::string horaPedido);
+    void setEstadoPreparacion(bool estado);
+    void setEstadoCompletado(bool estado);
+};
+
+#endif // PEDIDO_H
