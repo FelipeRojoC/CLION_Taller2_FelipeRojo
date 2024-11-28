46d5c2ef5d116ae2c679de678c855a92a17ec002
Felipoto <felipe.rojo@alumnos.ucn.cl>
2024-11-28 17:38:16 -0300

    feat: se agrega las validaciones en el menu

--- SistemaPedido.cpp
+++ SistemaPedido.cpp
@@ -53,7 +53,7 @@ void SistemaPedido::buscarPedido(int id) const {
     Pedido pedido = avlPedidos.buscarPedido(id);
     if (pedido.getId() != -1) {
         std::cout << "Pedido encontrado con ID " << pedido.getId() << "\n";
-        std::cout << "Cliente " << pedido.getNombreCliente() << " " << pedido.getApellidoCliente() << "\n";
+        std::cout << "Cliente " << pedido.getNombreCliente() << "q " << pedido.getApellidoCliente() << "\n";
         std::cout << "Productos ";
         for (const auto& producto : pedido.getProductos()) {
             std::cout << producto << ", ";
@@ -76,61 +76,81 @@ void SistemaPedido::cerrarSistema() {
 void SistemaPedido::mostrarMenu() {
     int opcion;
     do {
-        std::cout << "\n----- Sistema de pedidos Grill of Victory -----\n";
-        std::cout << "1 Crear Pedido\n";
-        std::cout << "2 Entregar Pedidos\n";
-        std::cout << "3 Cancelar Pedido\n";
-        std::cout << "4 Buscar Pedido\n";
-        std::cout << "5 Salir\n";
-        std::cout << "Seleccione una opcion ";
-        std::cin >> opcion;
+        try {
+            std::cout << "\n----- Sistema de pedidos Grill of Victory -----\n";
+            std::cout << "1 Crear Pedido\n";
+            std::cout << "2 Entregar Pedidos\n";
+            std::cout << "3 Cancelar Pedido\n";
+            std::cout << "4 Buscar Pedido\n";
+            std::cout << "5 Salir\n";
+            std::cout << "Seleccione una opcion ";
+            std::cin >> opcion;
 
-        //TODO poner un try catch para la opcion y hacer que la opcion se vuelva entero para validar y no se caiga
-        switch (opcion) {
-            case 1: {
-                std::cin.ignore(); //Limpiar el buffer
-                std::string nombre, apellido;
-                std::list<std::string> productos;
-                std::string producto;
-                int cantidad;
+            std::string entrada;
+            std::cin >> entrada;
 
-                std::cout << "Ingrese el nombre del cliente ";
-                std::getline(std::cin, nombre);
-                std::cout << "Ingrese el apellido del cliente ";
-                std::getline(std::cin, apellido);
-                std::cout << "Ingrese la cantidad de productos ";
-                std::cin >> cantidad;
-                std::cin.ignore();
-                for (int i = 0; i < cantidad; ++i) {
-                    std::cout << "Producto " << (i + 1) << "\n";
-                    std::getline(std::cin, producto);
-                    productos.push_back(producto);
+            //Convertir un numero la entrada
+            try {
+                opcion = std::stoi(entrada); //Convierte a un entero
+            } catch (const std::invalid_argument&) {
+                throw std::runtime_error("Entrada no valida, ingresa un numero ");
+            } catch (const std::out_of_range&) {
+                    throw std::runtime_error("Numero fuera del rango permitido ");
+            }
+
+            if (opcion < 1 || opcion > 5) {
+                throw std::runtime_error("Opcion no se encuentra en el menu, intente nuvamente ");
+            }
+
+            switch (opcion) {
+                case 1: {
+                    std::cin.ignore(); //Limpiar el buffer
+                    std::string nombre, apellido;
+                    std::list<std::string> productos;
+                    std::string producto;
+                    int cantidad;
+
+                    std::cout << "Ingrese el nombre del cliente ";
+                    std::getline(std::cin, nombre);
+                    std::cout << "Ingrese el apellido del cliente ";
+                    std::getline(std::cin, apellido);
+                    std::cout << "Ingrese la cantidad de productos ";
+                    std::cin >> cantidad;
+                    std::cin.ignore();
+                    for (int i = 0; i < cantidad; ++i) {
+                        std::cout << "Producto " << (i + 1) << " ";
+                        std::getline(std::cin, producto);
+                        productos.push_back(producto);
+                    }
+                    crearPedido(nombre, apellido, productos);
+                    break;
                 }
-                crearPedido(nombre, apellido, productos);
+                case 2:
+                    entregarPedidos();
                 break;
+                case 3: {
+                    int id;
+                    std::cout << "Ingrese el ID del pedido a cancelar ";
+                    std::cin >> id;
+                    cancelarPedido(id);
+                    break;
+                }
+                case 4: {
+                    int id;
+                    std::cout << "Ingrese el ID del pedido a buscar ";
+                    std::cin >> id;
+                    buscarPedido(id);
+                    break;
+                }
+                case 5:
+                    cerrarSistema();
+                break;
+                default: ;
             }
-            case 2:
-                entregarPedidos();
-                break;
-            case 3: {
-                int id;
-                std::cout << "Ingrese el ID del pedido a cancelar ";
-                std::cin >> id;
-                cancelarPedido(id);
-                break;
-            }
-            case 4: {
-                int id;
-                std::cout << "Ingrese el ID del pedido a buscar ";
-                std::cin >> id;
-                buscarPedido(id);
-                break;
-            }
-            case 5:
-                cerrarSistema();
-                break;
-            default:
-                std::cout << "Intente nuevamente \n";
+        } catch (const std::exception& e) {
+            std::cout << "ERROR " << e.what() << "\n";
+            std::cin.clear();              //Limpia el estado de error de `cin`
+            std::cin.ignore(INT_MAX, '\n'); //Descarta cualquier entrada no valida
         }
     } while (opcion != 5);
 }
